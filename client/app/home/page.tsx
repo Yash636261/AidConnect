60108322e5970216da69e5fcf40ee8b62b61cf4e
Yash Suthar <98970491+Yash636261@users.noreply.github.com>
2024-12-01 23:19:49 +0530

    update warning

--- client/app/home/page.tsx
+++ client/app/home/page.tsx
@@ -43,6 +43,26 @@ const Dashboard = ({ data }: any) => {
   return (
     <main className="p-8 space-y-8 max-h-screen overflow-y-scroll bg-[#E9EDF4] dark:bg-gray-950">
       <h1 className="text-3xl font-bold mb-8">Dashboard</h1>
+      <div className="flex items-center space-x-4 p-4 bg-yellow-100 border-l-4 border-yellow-500 text-yellow-700">
+        <svg
+          className="w-6 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M13 16h-1v-4h-1m0 0V9a4 4 0 118 0v3m-4 4h.01M12 20h.01M8 20h.01M16 20h.01M9 16h.01M15 16h.01M19 16h.01M5 16h.01"
+          />
+        </svg>
+        <div>
+          <p className="font-bold">Chennai Floods</p>
+          <p>Heavy rains!!</p>
+        </div>
+      </div>
       <Metrics data={data} />
       <div className="grid gap-8 md:grid-cols-2">
         <NeedsChart needs={data.needs} />
